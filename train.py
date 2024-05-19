c977b94938b13ac556456fc22f87f5c92a586b12
giordanovitale <giordi.vitale@gmail.com>
2024-05-19 11:11:52 +0200

    Class imbalance

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from sklearn.pipeline import make_pipeline
 
 data = pd.read_csv("data/data.csv")
 
-pipe = make_pipeline(StandardScaler(), LogisticRegression())
+pipe = make_pipeline(StandardScaler(), LogisticRegression(class_weight='balanced'))
 X = data.iloc[:,1:-1]
 y = data.iloc[:,-1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
