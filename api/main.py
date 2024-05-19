3198ed73cb8fdf7b61bb583f0a973c05ada040ca
Pietro <padovesepietro1999@gmail.com>
2024-05-19 11:04:55 +0200

    add io library

--- api/main.py
+++ api/main.py
@@ -88,8 +88,7 @@ async def predict(file: UploadFile = File(...)):
 async def visualize(file: UploadFile = File(...)):
     try:
         contents = await file.read()
-        print(type(contents))
-        df = pd.read_csv(contents)
+        df = pd.read_csv(io.StringIO(contents.decode("utf-8")))
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Error reading CSV file: {e}")
     
